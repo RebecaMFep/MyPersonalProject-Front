6bdbb23fe27c04084ff3e4818882277e9091fc79
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-03-25 00:22:15 +0100

    created EditFormView

---
+++ MyPersonalProject/src/views/EditFormView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import EditForm from '@/components/adminDashboard/EditForm.vue';
+
+</script>
+
+<template>
+  <main>
+    <div>  
+      <EditForm />
+    </div>
+</main>
+</template>
+
+<style lang="scss" scoped>
+</style>
