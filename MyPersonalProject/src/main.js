0baa63e650a55210eb75961d385b851a097e5950
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-03-24 19:39:48 +0100

    added icons to main.js

--- MyPersonalProject/src/main.js
+++ MyPersonalProject/src/main.js
@@ -1,46 +1,36 @@
-// import './assets/scss/main.scss'
+import './assets/scss/main.scss'
 
-// import { createApp } from 'vue'
-// import { createPinia } from 'pinia'
-
-// import App from './App.vue'
-// import router from './router'
-
-// import 'vuetify/styles'
-// import { createVuetify } from 'vuetify'
-// import * as components from 'vuetify/components'
-// import * as directives from 'vuetify/directives'
-
-// const vuetify = createVuetify({
-//     components,
-//     directives,
-//   })
-
-// const app = createApp(App)
-
-// app.use(vuetify)
-// app.use(createPinia())
-// app.use(router)
-
-// app.mount('#app')
+import '@fortawesome/fontawesome-free/css/all.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
-  components,
-  directives,
-})
+    components,
+    directives,
+    icons: {
+      defaultSet: 'fa',
+      aliases,
+      sets: {
+        fa,
+      },
+    },
+  })
 
 const app = createApp(App)
 
-app.use(vuetify)
+app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
