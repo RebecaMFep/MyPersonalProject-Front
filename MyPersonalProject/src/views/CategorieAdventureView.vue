272f7e90a93454310c58a7371f97ad4b85e093fa
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-03-25 02:33:30 +0100

    create categorie adventure view

--- MyPersonalProject/src/views/CategorieAdventureView.vue
+++ MyPersonalProject/src/views/CategorieAdventureView.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { useActivityStore } from "@/stores/activity";
-import Carusel from "@/components/home-components/Carousel.vue";
-import Bunner from "@/components/header-footer/Bunner.vue";
-import CategoriesSection from "@/components/home-components/CategoriesSection.vue";
+import BunnerAdventure from "../components/home-components/BunnerAdventure.vue";
 
 
 const store = useActivityStore()
@@ -11,31 +9,24 @@ store.getActivities()
 </script>
 
 <template>
-  <Bunner />
 
-  <section>
-    <div>
-      <h1>¿Quiénes somos?</h1>
-      <h3>
-        “Somos un equipo formado por apasionados profesionales dedicados a ofrecer experiencias emocionantes y enriquecedoras para niños durante los meses de julio y agosto en Gijón.”
-      </h3>
-    </div>
-    <div>
-      <h1>
-        ¿Qué ofrecemos?
-      </h1>
-
-      <h3>
-        “Tenemos el compromiso de proporcionar un entorno seguro, emocionante y educativo donde los niños puedan explorar, aprender y divertirse al máximo.  Nos enorgullece ofrecer una amplia gama de actividades diseñadas para fomentar la creatividad, el desarrollo físico y emocional, y la diversión al aire libre.”
-      </h3>
-    </div>
-  </section>
   <div>
-    <CategoriesSection />
+    <BunnerAdventure />
   </div>
-  <div> 
-    <Carusel :activities="store.activities"/>
+
+  <div class="title">
+    <h1>
+    ¡Explora nuestras actividades más interesantes!
+    </h1>
   </div>
+  <main>
+    <div v-for="activity in store.activities">
+      <CardProperty :activity="activity" />
+    </div>
+
+ </main>
+  
+ 
 
 </template>
 
@@ -43,22 +34,34 @@ store.getActivities()
 
 @import url('https://fonts.googleapis.com/css2?family=Lakki+Reddy&family=Ribeye+Marrow&display=swap');
 
-section {
+.title {
   display: flex;
-  text-align: center;
-  font-family: "Lakki Reddy", serif;
+  justify-content: center;
+  margin-top: 20px;
 }
 
 h1 {
   font-family: "Lakki Reddy", serif;
   color: #00A5B8; 
   font-size: 6rem;
+
 }
 
-h3 {
-  font-family: "Lakki Reddy", serif;
-  font-size: 3rem;
-  color: #555B6E;
+main {
+  display: flex;
+  justify-content: space-around;
+  margin: 5%;
+  flex-wrap: wrap;
+  gap:  10px; 
+ 
+  border-radius: 15px;
+
+}
+
+@media (max-width:  768px) {
+  .CardProperty {
+    width:  100%;
+  }
 }
 
 </style>
